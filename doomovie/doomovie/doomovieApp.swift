153eddff38e804c160f3bbdbfc594f2afb3bb6f7
altrumo <altrumo@yahoo.com>
2022-04-19 23:42:12 +0200

    first commit

---
+++ doomovie/doomovie/doomovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  doomovieApp.swift
+//  doomovie
+//
+//  Created by Alejandro Trujillo Moya on 19/4/22.
+//
+
+import SwiftUI
+
+@main
+struct doomovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
