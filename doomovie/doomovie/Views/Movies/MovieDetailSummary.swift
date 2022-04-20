61f9e813b2038c74cfa1c4f595ebc5bd57873e5e
altrumo <altrumo@yahoo.com>
2022-04-20 14:14:16 +0200

    fixed error if no overview text

--- doomovie/doomovie/Views/Movies/MovieDetailSummary.swift
+++ doomovie/doomovie/Views/Movies/MovieDetailSummary.swift
@@ -10,9 +10,11 @@ import SwiftUI
 struct MovieDetailSummary: View {
     var movie: Movie
     var body: some View {
-        VStack(alignment: .leading, spacing: 30){
-            DooText(text: "summary".localized(), color: .onSurfaceMedium, size: 24, weight: .bold)
-            DooText(text: movie.overview ?? "---", color: .onSurface).frame(maxWidth: UIScreen.main.bounds.width-60)
-        }.padding(.leading, 20)
+        if(movie.overview != ""){
+            VStack(alignment: .leading, spacing: 30){
+                DooText(text: "summary".localized(), color: .onSurfaceMedium, size: 24, weight: .bold)
+                DooText(text: movie.overview ?? "---", color: .onSurface).frame(maxWidth: UIScreen.main.bounds.width-60)
+            }.padding(.leading, 20)
+        }
     }
 }
